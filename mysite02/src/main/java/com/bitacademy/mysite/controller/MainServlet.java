ffb640cba1032c6d1cffd5dfd554346f45a354e8
devanglefish <jaebeommun@gmail.com>
2020-12-22 17:32:38 +0900

    mysite02 #2

---
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/MainServlet.java
@@ -0,0 +1,28 @@
+package com.bitacademy.mysite.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bitacademy.web.mvc.WebUtil;
+
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public MainServlet() {
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		WebUtil.forward(request, response, "/WEB-INF/views/main/index.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
