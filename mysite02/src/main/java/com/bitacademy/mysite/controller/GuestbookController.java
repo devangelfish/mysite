c2fd70adf59b751b8fd1ae9f93fb9d6bc897eec3
devanglefish <jaebeommun@gmail.com>
2020-12-24 11:33:45 +0900

    Encoding Filter

--- mysite02/src/main/java/com/bitacademy/mysite/controller/GuestbookController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/GuestbookController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bitacademy.mysite.mvc.guestbook.GuestActionFactory;
-import com.bitacademy.mysite.mvc.main.MainActionFactory;
 import com.bitacademy.web.mvc.Action;
 import com.bitacademy.web.mvc.ActionFactory;
 
@@ -16,8 +15,6 @@ public class GuestbookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-
 		String actionName = request.getParameter("a");
 
 		ActionFactory actionFactory = new GuestActionFactory();
--- mysite02/src/main/java/com/bitacademy/mysite/web/listener/ContextLoadListener.java
+++ mysite02/src/main/java/com/bitacademy/mysite/web/listener/ContextLoadListener.java
@@ -1,5 +1,6 @@
 package com.bitacademy.mysite.web.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -13,7 +14,10 @@ public class ContextLoadListener implements ServletContextListener {
         
     }
 
-    public void contextInitialized(ServletContextEvent arg0)  { 
-         System.out.println("Application starts....");
+    public void contextInitialized(ServletContextEvent servletContextEvent)  { 
+    	ServletContext context = servletContextEvent.getServletContext();
+    	String contextConfigLocation = context.getInitParameter("contextConfigLocation");
+    	
+        System.out.println("Application starts...." + contextConfigLocation);
     }	
 }
