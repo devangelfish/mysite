741869b9152f89d4f3dc7b7116ab3bb05753cccb
devanglefish <jaebeommun@gmail.com>
2021-01-04 12:08:36 +0900

    Init mysite03/guestbook

---
+++ mysite03/src/main/java/com/bitacademy/mysite/service/UserService.java
@@ -0,0 +1,10 @@
+package com.bitacademy.mysite.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+}
